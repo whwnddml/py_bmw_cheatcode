3a8f4d6e28784b1b6d317b374106d92b3759cb37
jecho <jecho@bluetechs.com>
2021-06-03 16:06:12 +0900

    불필요한 코드 제거, 주석 추가

--- cheatKey.py
+++ cheatKey.py
@@ -2,9 +2,10 @@ from xml.etree.ElementTree import Element,SubElement,ElementTree,dump
 #-*-coding:utf-8 -*-
 
 import pandas as pd
+# 출력할 xml 파일명(확장자 제외)
+filename = "bmw-codes"
 
-filename = "bmw-codeskk"
-
+# 읽을 엑셀파일명(확장자 포함)
 #xlsx = pd.read_excel('./bmw-codes.xlsx',sheet_name="Sheet1")
 #xlsx = pd.read_excel('./bmw-codes.xlsx')
 xlsx = pd.read_excel('./(BMW) G30 코딩좌표.xlsx')
@@ -14,7 +15,7 @@ xlsx = pd.read_excel('./(BMW) G30 코딩좌표.xlsx')
 root = Element('FDL')
 cafd_id = "NULL"
 
-
+# 로우단위로 반복하여 데이터 만들기
 for index, row in xlsx.iterrows():
     if row['cafd_id'] != cafd_id:
         name1 = SubElement(root, 'cafd')
@@ -37,42 +38,7 @@ for index, row in xlsx.iterrows():
 
     cafd_id = row['cafd_id']
 
-
-
-#
-# for index, row in xlsx.iterrows():
-#     if cafd_id != "NULL" and row['cafd_id'] == cafd_id:
-#         name2 = SubElement(name1, 'code')
-#         name2.set("description", row['description'])
-#         name3 = SubElement(name2, "group")
-#         name3.set("id", str(row['group_id']))
-#         name4 = SubElement(name3, "function")
-#         name4.set("comment", row['comment'])
-#         name4.set("mask", str(row['mask']))
-#         name4.set("start", str(row['start']))
-#         name4.set("end", str(row['end']))
-#         name4.text = str(row['function'])
-#         continue
-#     name1 = SubElement(root, 'cafd')
-#     name1.set("id", row['cafd_id'])
-#     name1.set("name", row['module'])
-#     #name1.set("series", row['series'])
-#     name1.set("author", row['author'])
-#
-#     name2 = SubElement(name1, 'code')
-#     name2.set("description", row['description'])
-#     name3 = SubElement(name2, "group")
-#     name3.set("id", str(row['group_id']))
-#     name4 = SubElement(name3, "function")
-#     name4.set("comment", row['comment'])
-#     name4.set("mask", str(row['mask']))
-#     name4.set("start", str(row['start']))
-#     name4.set("end", str(row['end']))
-#     name4.text = str(row['function'])
-#
-#     cafd_id = row['cafd_id']
-#
-
+# 만들어진 데이터를 xml 생성
 dump(root)
 
 tree = ElementTree(root)
